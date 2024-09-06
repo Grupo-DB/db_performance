db8c28b851522fd5a10f0bd890234e3c17f85cde
Grupo DB <dbrepositorio@outlook.com>
2024-09-06 18:03:16 -0300

    dia 06/09

--- bisGerenciais/dashboardOperacoes/britagem/views.py
+++ bisGerenciais/dashboardOperacoes/britagem/views.py
@@ -27,8 +27,8 @@ def calculos_britagem(request):
         data_inicio = datetime.now().strftime('%Y-%m-01 00:00:00')  # Início do mês
         data_fim = datetime.now().strftime('%Y-%m-%d 23:59:59')  # Data atual
     elif tipo_calculo == 'anual':
-        data_inicio = datetime.now().strftime('%Y-01-01 00:00:00')  # Início do ano
-        data_fim = datetime.now().strftime('%Y-%m-%d 23:59:59')  # Data atual
+        data_inicio = datetime.now().strftime('%Y-01-01 07:10:00')  # Início do ano
+        data_fim = datetime.now().strftime('%Y-%m-%d 07:10:00')  # Data atual
     else:
         return JsonResponse({'error': 'Tipo de cálculo inválido'}, status=400)
     
@@ -162,7 +162,124 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
         else:
             setup_tempo = f'{int(minutes)}m'
     else:    
-        setup_tempo = '0m'    
+        setup_tempo = '0m'  
+
+
+###########--FALTA MATERIA PRIMA------- ######################
+    des = consulta_evento_parada.loc[consulta_evento_parada['EDPREVD']==14,['TEMPO','PERC_TOTAL']]
+    materiaprima_percentual = des['PERC_TOTAL'].values
+    if materiaprima_percentual != 0:
+        materiaprima_percentual = ', '.join(map(str, materiaprima_percentual))
+        materiaprima_percentual = round(float(materiaprima_percentual),1)
+    else:
+        materiaprima_percentual = 0
+    materiaprima_tempo = des['TEMPO'].values
+    if materiaprima_tempo != 0:
+        materiaprima_tempo = ', '.join(map(str, materiaprima_tempo))
+        materiaprima_tempo_hours = float(materiaprima_tempo) / 3600
+        total_minutes = round(materiaprima_tempo_hours * 60)
+
+         # Calcula horas e minutos restantes
+        hours, minutes = divmod(total_minutes, 60)
+        if hours > 0:
+            materiaprima_tempo = f'{int(hours)}h {int(minutes)}m'  # Converte para int para evitar decimais
+        else:
+            materiaprima_tempo = f'{int(minutes)}m'
+    else:    
+        materiaprima_tempo = '0m'    
+
+###########--ESPERANDO DEMANDA------- ######################
+    des = consulta_evento_parada.loc[consulta_evento_parada['EDPREVD']==12,['TEMPO','PERC_TOTAL']]
+    esperando_demanda_percentual = des['PERC_TOTAL'].values
+    if esperando_demanda_percentual != 0:
+        esperando_demanda_percentual = ', '.join(map(str, esperando_demanda_percentual))
+        esperando_demanda_percentual = round(float(esperando_demanda_percentual),1)
+    else:
+        esperando_demanda_percentual = 0
+    esperando_demanda_tempo = des['TEMPO'].values
+    if esperando_demanda_tempo != 0:
+        esperando_demanda_tempo = ', '.join(map(str, esperando_demanda_tempo))
+        esperando_demanda_tempo_hours = float(esperando_demanda_tempo) / 3600
+        total_minutes = round(esperando_demanda_tempo_hours * 60)
+
+         # Calcula horas e minutos restantes
+        hours, minutes = divmod(total_minutes, 60)
+        if hours > 0:
+            esperando_demanda_tempo = f'{int(hours)}h {int(minutes)}m'  # Converte para int para evitar decimais
+        else:
+            esperando_demanda_tempo = f'{int(minutes)}m'
+    else:    
+        esperando_demanda_tempo = '0m'  
+
+###########--PREPARANDO LOCAL------- ######################
+    des = consulta_evento_parada.loc[consulta_evento_parada['EDPREVD']==9,['TEMPO','PERC_TOTAL']]
+    preparando_local_percentual = des['PERC_TOTAL'].values
+    if preparando_local_percentual != 0:
+        preparando_local_percentual = ', '.join(map(str, preparando_local_percentual))
+        preparando_local_percentual = round(float(preparando_local_percentual),1)
+    else:
+        preparando_local_percentual = 0
+    preparando_local_tempo = des['TEMPO'].values
+    if preparando_local_tempo != 0:
+        preparando_local_tempo = ', '.join(map(str, preparando_local_tempo))
+        preparando_local_tempo_hours = float(preparando_local_tempo) / 3600
+        total_minutes = round(preparando_local_tempo_hours * 60)
+
+         # Calcula horas e minutos restantes
+        hours, minutes = divmod(total_minutes, 60)
+        if hours > 0:
+            preparando_local_tempo = f'{int(hours)}h {int(minutes)}m'  # Converte para int para evitar decimais
+        else:
+            preparando_local_tempo = f'{int(minutes)}m'
+    else:    
+        preparando_local_tempo = '0m'
+
+###########--ALIMENTADOR DESLIGADO------- ######################
+    des = consulta_evento_parada.loc[consulta_evento_parada['EDPREVD']==53,['TEMPO','PERC_TOTAL']]
+    alimentador_desligado_percentual = des['PERC_TOTAL'].values
+    if alimentador_desligado_percentual != 0:
+        alimentador_desligado_percentual = ', '.join(map(str, alimentador_desligado_percentual))
+        alimentador_desligado_percentual = round(float(alimentador_desligado_percentual),1)
+    else:
+        alimentador_desligado_percentual = 0
+    alimentador_desligado_tempo = des['TEMPO'].values
+    if alimentador_desligado_tempo != 0:
+        alimentador_desligado_tempo = ', '.join(map(str, alimentador_desligado_tempo))
+        alimentador_desligado_tempo_hours = float(alimentador_desligado_tempo) / 3600
+        total_minutes = round(alimentador_desligado_tempo_hours * 60)
+
+         # Calcula horas e minutos restantes
+        hours, minutes = divmod(total_minutes, 60)
+        if hours > 0:
+            alimentador_desligado_tempo = f'{int(hours)}h {int(minutes)}m'  # Converte para int para evitar decimais
+        else:
+            alimentador_desligado_tempo = f'{int(minutes)}m'
+    else:    
+        alimentador_desligado_tempo = '0m'
+
+###########--EVENTO NAO INFORMADO------- ######################
+    des = consulta_evento_parada.loc[consulta_evento_parada['EDPREVD']==28,['TEMPO','PERC_TOTAL']]
+    evento_nao_informado_percentual = des['PERC_TOTAL'].values
+    if evento_nao_informado_percentual != 0:
+        evento_nao_informado_percentual = ', '.join(map(str, evento_nao_informado_percentual))
+        evento_nao_informado_percentual = round(float(evento_nao_informado_percentual),1)
+    else:
+        evento_nao_informado_percentual = 0
+    evento_nao_informado_tempo = des['TEMPO'].values
+    if evento_nao_informado_tempo != 0:
+        evento_nao_informado_tempo = ', '.join(map(str, evento_nao_informado_tempo))
+        evento_nao_informado_tempo_hours = float(evento_nao_informado_tempo) / 3600
+        total_minutes = round(evento_nao_informado_tempo_hours * 60)
+
+         # Calcula horas e minutos restantes
+        hours, minutes = divmod(total_minutes, 60)
+        if hours > 0:
+            evento_nao_informado_tempo = f'{int(hours)}h {int(minutes)}m'  # Converte para int para evitar decimais
+        else:
+            evento_nao_informado_tempo = f'{int(minutes)}m'
+    else:    
+        evento_nao_informado_tempo = '0m'
+
 
 ################################################################################################
 
@@ -289,6 +406,7 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
         DTRSIT = 1
         AND DTREMP = 1
         AND DTRFIL = 0
+        
         AND CAST(DTRDATA1 AS DATE) BETWEEN '{data_inicio}' AND '{data_fim}'
         
     ORDER BY 
@@ -296,29 +414,61 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
             """,connections[connection_name])
     
     ###KPI´S
-    #mb_me = consulta_mov.loc[(consulta_mov['LOCCOD_L3']==1) & (consulta_mov['LOCCOD_DESTINO_L3']==10)]
-    mb_me = consulta_mov.loc[
-    (consulta_mov['LOCCOD_L3'] == 1) & 
+    ##############################-----------------MINA---BRITADOR---------------------###########################
+    mina_britador = consulta_mov.loc[
+    (consulta_mov['LOCCOD_L3'] == 1) &
     (
         (consulta_mov['LOCCOD_DESTINO_L3'] == 10) |
         (consulta_mov['LOCCOD_DESTINO_L2'] == 10) |
         (consulta_mov['LOCCOD_DESTINO_L1'] == 10)
     )
 ]
-#     mb_me = consulta_mov.loc[
-#     (consulta_mov['LOCCOD_L3'].astype(str).str.contains('1', case=False, na=False) & 
-#      consulta_mov['LOCCOD_DESTINO_L3'].astype(str)=='10') 
-#    # consulta_mov['LOCCOD_DESTINO_L2'].astype(str)=='10' 
-#     #consulta_mov['LOCCOD_DESTINO_L1'].astype(str).str.contains('10', case=False, na=False)
-# ]
+    mina_britador = round(mina_britador['PESO'].sum(),1)
+    mina_britador = locale.format_string("%.0f",mina_britador,grouping=True)
 
-    
-    tot_tn_mn_brit_me = round(mb_me['PESO'].sum(),1)
-    tot_tn_mn_brit_me = locale.format_string("%.2f",tot_tn_mn_brit_me,grouping=True)
+    ####################----MINA ESTOQUE---------------------####################################
+    mina_estoque = consulta_mov.loc[
+    (consulta_mov['LOCCOD_L3'] == 1) &
+        (
+            (consulta_mov['LOCCOD_DESTINO_L3'] == 21) |
+            (consulta_mov['LOCCOD_DESTINO_L2'] == 21) |
+            (consulta_mov['LOCCOD_DESTINO_L1'] == 21) 
+        )
+]
+    mina_estoque = round(mina_estoque['PESO'].sum(),1)
+    mina_estoque = locale.format_string("%.0f",mina_estoque,grouping=True)
 
+        ####################----ESTOQUE--BRITADOR---------------------####################################
+    estoque_britador = consulta_mov.loc[
+    (
+        (consulta_mov['LOCCOD_L2'].isin([10, 21])) | 
+        (consulta_mov['LOCCOD_L1'].isin([10, 21])) | 
+        (consulta_mov['LOCCOD_L3'].isin([10, 21]))
+    ) &
+    (
+        (consulta_mov['LOCCOD_DESTINO_L3'] == 10) |
+        (consulta_mov['LOCCOD_DESTINO_L2'] == 10) |
+        (consulta_mov['LOCCOD_DESTINO_L1'] == 10)
+    ) &
+    (
+        (consulta_mov['PPROCOD'].isin([1,2,21]))
+    )
+]
+    estoque_britador = round(estoque_britador['PESO'].sum(),1)
+    estoque_britador = locale.format_string("%.0f",estoque_britador,grouping=True)
 ################---------CONTEXT--------------###########################
 
     response_data = {
+        'alimentador_desligado_percentual':alimentador_desligado_percentual,
+        'alimentador_desligado_tempo':alimentador_desligado_tempo,
+        'evento_nao_informado_percentual':evento_nao_informado_percentual,
+        'evento_nao_informado_tempo':evento_nao_informado_tempo,
+        'preparando_local_percentual':preparando_local_percentual,
+        'preparando_local_tempo':preparando_local_tempo,
+        'esperando_demanda_percentual':esperando_demanda_percentual,
+        'esperando_demanda_tempo':esperando_demanda_tempo,
+        'materiaprima_percentual':materiaprima_percentual,
+        'materiaprima_tempo':materiaprima_tempo,
         'setup_percentual':setup_percentual,
         'setup_tempo':setup_tempo,
         'embuchamento_desarme_percentual':embuchamento_desarme_percentual,
@@ -327,7 +477,10 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
         'embuchamento_rompedor_tempo':embuchamento_rompedor_tempo,
         'almoco_janta_percentual':almoco_janta_percentual,
         'almoco_janta_tempo': almoco_janta_tempo,
-        'tot_tn_mn_brit_me':tot_tn_mn_brit_me
+        'mina_britador':mina_britador,
+        'mina_estoque':mina_estoque,
+        'estoque_britador':estoque_britador,
+
     }
 
     return JsonResponse(response_data, safe=False)        
