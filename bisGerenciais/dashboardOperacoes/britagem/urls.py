88b1cb1cef51a8da6d3eed3b1789336e74712a00
Grupo DB <dbrepositorio@outlook.com>
2024-09-12 23:20:39 -0300

    dia 12/09

--- bisGerenciais/dashboardOperacoes/britagem/urls.py
+++ bisGerenciais/dashboardOperacoes/britagem/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import routers
 from . import views
-from .views import calculos_britagem
+from .views import calculos_britagem,calculos_graficos
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenVerifyView
 from rest_framework.routers import DefaultRouter
 
@@ -13,4 +13,5 @@ router.register(r'home', calculos_britagem, basename='Calcario')
 
 urlpatterns = [
     path('calcular_britagem/', calculos_britagem, name='britagem'),
+    path('calcular_graficos/',calculos_graficos,name= 'graficos')
 ]
--- bisGerenciais/dashboardOperacoes/britagem/views.py
+++ bisGerenciais/dashboardOperacoes/britagem/views.py
@@ -31,6 +31,7 @@ def calculos_britagem(request):
         data_fim = datetime.now().strftime('%Y-%m-%d 07:10:00')  # Data atual
     else:
         return JsonResponse({'error': 'Tipo de cálculo inválido'}, status=400)
+
     
     consulta_evento_parada = pd.read_sql(f"""
 
@@ -416,11 +417,18 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
     ###KPI´S
     ##############################-----------------MINA---BRITADOR---------------------###########################
     mina_britador = consulta_mov.loc[
-    (consulta_mov['LOCCOD_L3'] == 1) &
+    (
+        (consulta_mov['LOCCOD_L3'] == 1) |
+        (consulta_mov['LOCCOD_L2'] == 1) |
+        (consulta_mov['LOCCOD_L1'] == 1) 
+    )  &
     (
         (consulta_mov['LOCCOD_DESTINO_L3'] == 10) |
         (consulta_mov['LOCCOD_DESTINO_L2'] == 10) |
         (consulta_mov['LOCCOD_DESTINO_L1'] == 10)
+    ) &
+    (
+        (consulta_mov['PPROCOD'].isin([1,2,21]))
     )
 ]
     mina_britador = round(mina_britador['PESO'].sum(),1)
@@ -428,12 +436,19 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
 
     ####################----MINA ESTOQUE---------------------####################################
     mina_estoque = consulta_mov.loc[
-    (consulta_mov['LOCCOD_L3'] == 1) &
+    (
+        (consulta_mov['LOCCOD_L3'] == 1) |
+        (consulta_mov['LOCCOD_L2'] == 1) |
+        (consulta_mov['LOCCOD_L1'] == 1)
+    )    &
         (
-            (consulta_mov['LOCCOD_DESTINO_L3'] == 21) |
-            (consulta_mov['LOCCOD_DESTINO_L2'] == 21) |
-            (consulta_mov['LOCCOD_DESTINO_L1'] == 21) 
-        )
+            (consulta_mov['LOCCOD_DESTINO_L3'].isin([10,21])) |
+            (consulta_mov['LOCCOD_DESTINO_L2'].isin([10,21])) |
+            (consulta_mov['LOCCOD_DESTINO_L1'].isin([10,21])) 
+        ) &
+    (
+        (consulta_mov['PPROCOD'].isin([1,2,21]))
+    )
 ]
     mina_estoque = round(mina_estoque['PESO'].sum(),1)
     mina_estoque = locale.format_string("%.0f",mina_estoque,grouping=True)
@@ -456,6 +471,28 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
 ]
     estoque_britador = round(estoque_britador['PESO'].sum(),1)
     estoque_britador = locale.format_string("%.0f",estoque_britador,grouping=True)
+
+###########--------MINA---REJEITO---------------###############################################################
+
+    mina_rejeito = consulta_mov.loc[
+        (
+            (consulta_mov['LOCCOD_L1'].isin([1,10,15,16,17,19,20,21,22,63,92])) |
+            (consulta_mov['LOCCOD_L2'].isin([1,10,15,16,17,19,20,21,22,63,92])) |
+            (consulta_mov['LOCCOD_L3'].isin([1,10,15,16,17,19,20,21,22,63,92])) 
+        ) &
+        (
+            (consulta_mov['LOCCOD_DESTINO_L1'].isin([1,10,15,16,17,19,20,21,22,63,92])) |
+            (consulta_mov['LOCCOD_DESTINO_L2'].isin([1,10,15,16,17,19,20,21,22,63,92])) |
+            (consulta_mov['LOCCOD_DESTINO_L3'].isin([1,10,15,16,17,19,20,21,22,63,92]))
+        ) &
+        (
+            (consulta_mov['PPROCOD'] == 3)
+        )
+    ]
+    mina_rejeito = round(mina_rejeito['PESO'].sum(),1)
+    mina_rejeito = locale.format_string("%.0f",mina_rejeito,grouping=True)
+
+    
 ################---------CONTEXT--------------###########################
 
     response_data = {
@@ -480,7 +517,314 @@ GROUP BY EDPREVD, EVDNOME, EDPROPERSN, DPREQP
         'mina_britador':mina_britador,
         'mina_estoque':mina_estoque,
         'estoque_britador':estoque_britador,
-
+        'mina_rejeito':mina_rejeito
     }
 
     return JsonResponse(response_data, safe=False)        
+
+######################################################################------------GRÁFICOS-------------------------------######################################################################
+@csrf_exempt
+@api_view(['POST'])
+def calculos_graficos(request):
+    connection_name = 'sga'
+    
+    # Recuperando o tipo de cálculo do corpo da requisição
+    tipo_calculo = request.data.get('tipo_calculo')
+
+    # Definindo as datas com base no tipo de cálculo
+    if tipo_calculo == 'atual':
+        data_inicio = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d %H:%M:%S')
+        data_fim = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S')
+    elif tipo_calculo == 'mensal':
+        data_inicio = datetime.now().strftime('%Y-%m-01 00:00:00')  # Início do mês
+        data_fim = datetime.now().strftime('%Y-%m-%d 23:59:59')  # Data atual
+    elif tipo_calculo == 'anual':
+        data_inicio = datetime.now().strftime('%Y-01-01 00:00:00')  # Início do ano
+        data_fim = datetime.now().strftime('%Y-%m-%d 23:59:59')  # Data atual
+    else:
+        return JsonResponse({'error': 'Tipo de cálculo inválido'}, status=400)
+
+    # Consulta SQL
+    consulta_volume_britado = pd.read_sql(f""" 
+        SELECT 0 TIPO, DPRCOD, DPRREF, EQPCOD, EQPNOME, LOCCOD, LOCNOME, SUM(ADPRPESOTOT) TOTAL, DPRHROPER, DPRDATA1
+        FROM ALIMDIARIAPROD
+        JOIN DIARIAPROD ON DPRCOD = ADPRDPR
+        JOIN LOCAL LD ON LD.LOCCOD = ADPRLOC
+        JOIN EQUIPAMENTO ON EQPCOD = DPREQP
+        WHERE DPRSIT = 1
+        AND DPREMP = 1
+        AND DPRFIL = 0
+        AND CAST(DPRDATA1 AS DATE) BETWEEN '{data_inicio}' AND '{data_fim}'
+        AND ADPRLOC <> 0
+        GROUP BY EQPNOME, EQPCOD, LOCCOD, LOCNOME, DPRCOD, DPRREF, DPRHROPER, DPRDATA1
+
+        UNION
+
+        SELECT 0 TIPO, DPRCOD, DPRREF, EQPCOD, EQPNOME, LOCCOD, LOCNOME, SUM(ADTRPESOTOT) TOTAL, DPRHRPROD, DTRDATA1
+        FROM ALIMDIARIATRANSP
+        JOIN ITEMDIARIATRANSP ON IDTRCOD = ADTRIDTR
+        JOIN DIARIATRANSP ON DTRCOD = IDTRDTR
+        JOIN LOCAL LD ON LD.LOCCOD = ADTRLOC
+        JOIN DIARIAPROD ON DPRCOD = IDTRDPR
+        JOIN EQUIPAMENTO ON EQPCOD = DPREQP
+        WHERE DTRSIT = 1
+        AND CAST(DTRDATA1 AS DATE) BETWEEN '{data_inicio}' AND '{data_fim}'
+        AND IDTRTIPODEST = 1
+        AND DTREMP = 1
+        AND DTRFIL = 0
+        GROUP BY EQPNOME, EQPCOD, LOCCOD, LOCNOME, DPRCOD, DPRREF, DPRHRPROD, DTRDATA1
+
+        ORDER BY 5,7
+    """, connections[connection_name])
+
+    # Inicializar variáveis
+    volume_diario = None
+    volume_mensal = None
+
+    # Verificar se 'DPRDATA1' está presente e não é nulo
+    if 'DPRDATA1' in consulta_volume_britado.columns:
+        consulta_volume_britado['DPRDATA1'] = pd.to_datetime(consulta_volume_britado['DPRDATA1'], errors='coerce')
+        consulta_volume_britado = consulta_volume_britado.dropna(subset=['DPRDATA1'])  # Remover linhas onde 'DPRDATA1' é nulo
+
+        # Quebrando o cálculo mensal em dias
+        if tipo_calculo == 'mensal':
+            consulta_volume_britado['DIA'] = consulta_volume_britado['DPRDATA1'].dt.day
+            #volume_diario = consulta_volume_britado.groupby('DIA')['TOTAL'].sum().reset_index()
+
+            # Calculando volume diário para LOCCOD 44 e 62
+            volume_diario_loc_44 = consulta_volume_britado[consulta_volume_britado['LOCCOD'] == 44].groupby('DIA')['TOTAL'].sum().reset_index()
+            volume_diario_loc_62 = consulta_volume_britado[consulta_volume_britado['LOCCOD'] == 62].groupby('DIA')['TOTAL'].sum().reset_index()
+            
+        
+            # Calculando a média mensal para Pedra Calcario
+            media_diaria_calcario = volume_diario_loc_44['TOTAL'].mean()
+            media_diaria_calcario = locale.format_string("%.0f", media_diaria_calcario, grouping=True)
+
+            # Calculando a média mensal para Pedra Cal
+            media_diaria_cal = volume_diario_loc_62['TOTAL'].mean()
+            media_diaria_cal = locale.format_string("%.0f", media_diaria_cal, grouping=True)
+
+            # Calculando projeção
+            dias_corridos = consulta_volume_britado['DIA'].max()  # Último dia do mês em que houve produção
+            dias_no_mes = (consulta_volume_britado['DPRDATA1'].max().replace(day=1) + pd.DateOffset(months=1) - pd.DateOffset(days=1)).day
+
+            if dias_corridos > 0:
+                # Projeção Calcário (LOCCOD 44)
+                producao_acumulada_calcario = volume_diario_loc_44['TOTAL'].sum()
+                projecao_calcario = (producao_acumulada_calcario / dias_corridos) * dias_no_mes
+                projecao_calcario = locale.format_string("%.0f", projecao_calcario, grouping=True)
+
+                # Projeção Cal (LOCCOD 62)
+                producao_acumulada_cal = volume_diario_loc_62['TOTAL'].sum()
+                projecao_cal = (producao_acumulada_cal / dias_corridos) * dias_no_mes
+                projecao_cal = locale.format_string("%.0f", projecao_cal, grouping=True)
+            else:
+                projecao_calcario = 0
+                projecao_cal = 0
+
+            volume_diario = {
+                'LOCCOD_44': volume_diario_loc_44.to_dict(orient='records'),
+                'LOCCOD_62': volume_diario_loc_62.to_dict(orient='records'),
+                'media_diaria_calcario': media_diaria_calcario,
+                'media_diaria_cal': media_diaria_cal,
+                'projecao_calcario': projecao_calcario,
+                'projecao_cal': projecao_cal
+            }
+
+        # Quebrando o cálculo anual em meses
+        elif tipo_calculo == 'anual':
+            consulta_volume_britado['MES'] = consulta_volume_britado['DPRDATA1'].dt.month
+            #volume_mensal = consulta_volume_britado.groupby('MES')['TOTAL'].sum().reset_index()
+
+            # Calculando volume diário para LOCCOD 44 e 62
+            volume_mensal_loc_44 = consulta_volume_britado[consulta_volume_britado['LOCCOD'] == 44].groupby('MES')['TOTAL'].sum().reset_index()
+            volume_mensal_loc_62 = consulta_volume_britado[consulta_volume_britado['LOCCOD'] == 62].groupby('MES')['TOTAL'].sum().reset_index()
+            
+            # Calculando a média mensal para Pedra Calcario
+            media_mensal_calcario = volume_mensal_loc_44['TOTAL'].mean()
+            media_mensal_calcario = locale.format_string("%.0f", media_mensal_calcario, grouping=True)
+
+            # Calculando a média mensal para Pedra Cal
+            media_mensal_cal = volume_mensal_loc_62['TOTAL'].mean()
+            media_mensal_cal = locale.format_string("%.0f", media_mensal_cal, grouping=True)
+
+            # Projeção anual
+            meses_corridos = consulta_volume_britado['MES'].max()  # Último mês em que houve produção
+            meses_no_ano = 12  # Número total de meses no ano
+
+            if meses_corridos > 0:
+                # Projeção Calcário (LOCCOD 44)
+                producao_acumulada_calcario = volume_mensal_loc_44['TOTAL'].sum()
+                projecao_anual_calcario = (producao_acumulada_calcario / meses_corridos) * meses_no_ano
+                projecao_anual_calcario = locale.format_string("%.0f", projecao_anual_calcario, grouping=True)
+
+                # Projeção Cal (LOCCOD 62)
+                producao_acumulada_cal = volume_mensal_loc_62['TOTAL'].sum()
+                projecao_anual_cal = (producao_acumulada_cal / meses_corridos) * meses_no_ano
+                projecao_anual_cal = locale.format_string("%.0f", projecao_anual_cal, grouping=True)
+            else:
+                projecao_anual_calcario = 0
+                projecao_anual_cal = 0
+
+            volume_mensal = {
+                'LOCCOD_44': volume_mensal_loc_44.to_dict(orient='records'),
+                'LOCCOD_62': volume_mensal_loc_62.to_dict(orient='records'),
+                'media_mensal_calcario': media_mensal_calcario,
+                'media_mensal_cal': media_mensal_cal,
+                'projecao_anual_calcario': projecao_anual_calcario,
+                'projecao_anual_cal': projecao_anual_cal
+            }
+
+    # Calcular volumes por local
+    volume_britado_por_loc = consulta_volume_britado.groupby('LOCCOD')['TOTAL'].sum()
+    volume_britado_por_loc = volume_britado_por_loc.reindex([44, 62, 66], fill_value=0)
+
+    # Somando os valores dos códigos 44, 62 e 66
+    volume_britado_total = round(volume_britado_por_loc.loc[[44, 62, 66]].sum(), 2)
+    volume_britado_total = locale.format_string("%.0f", volume_britado_total, grouping=True)
+
+    # Calculando produção do britador (somando 44 e 62)
+    producao_britador = round(volume_britado_por_loc.loc[[44, 62]].sum(), 2)
+    producao_britador = locale.format_string("%.0f", producao_britador, grouping=True)
+
+    # Montando os dados de resposta
+    response_data = {
+        'volume_britado_total': volume_britado_total,
+        'producao_britador': producao_britador,
+    }
+
+    # Adicionando o volume diário se o tipo de cálculo for 'mensal'
+    if volume_diario is not None:
+        response_data['volume_diario'] = volume_diario
+
+    # Adicionando o volume mensal se o tipo de cálculo for 'anual'
+    if volume_mensal is not None:
+        response_data['volume_mensal'] = volume_mensal
+
+         
+
+    return JsonResponse(response_data)
+
+
+# @csrf_exempt
+# @api_view(['POST'])
+# def calculos_graficos(request):
+#     connection_name = 'sga'
+    
+#     # Recuperando o tipo de cálculo do corpo da requisição
+#     tipo_calculo = request.data.get('tipo_calculo')
+
+#     # Definindo as datas com base no tipo de cálculo
+#     if tipo_calculo == 'atual':
+#         data_inicio = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d %H:%M:%S')
+#         data_fim = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S')
+
+#     elif tipo_calculo == 'mensal':
+#         # Quebrar o mensal em diário
+#         data_inicio = datetime.now().strftime('%Y-%m-01 00:00:00')  # Início do mês
+#         data_fim = datetime.now().strftime('%Y-%m-%d 23:59:59')  # Data atual
+        
+#         # Itera sobre os dias do mês
+#         data_inicio_dt = datetime.strptime(data_inicio, '%Y-%m-%d %H:%M:%S')
+#         data_fim_dt = datetime.strptime(data_fim, '%Y-%m-%d %H:%M:%S')
+#         resultados_diarios = []
+
+#         while data_inicio_dt <= data_fim_dt:
+#             dia_fim = data_inicio_dt + timedelta(days=1)
+#             consulta_volume_britado = pd.read_sql(f"""
+#                 SELECT 0 TIPO, DPRCOD, DPRREF, EQPCOD, EQPNOME, LOCCOD, LOCNOME, SUM(ADPRPESOTOT) TOTAL, (DPRHROPER) TOTAL_HORAS FROM ALIMDIARIAPROD
+#                 JOIN DIARIAPROD ON DPRCOD = ADPRDPR
+#                 JOIN LOCAL LD ON LD.LOCCOD = ADPRLOC
+#                 JOIN EQUIPAMENTO ON EQPCOD = DPREQP
+#                 WHERE DPRSIT = 1
+#                 AND DPREMP = 1
+#                 AND DPRFIL = 0
+#                 AND CAST(DPRDATA1 as date) BETWEEN '{data_inicio_dt.strftime('%Y-%m-%d')}' AND '{dia_fim.strftime('%Y-%m-%d')}'
+#                 AND ADPRLOC <> 0
+#                 GROUP BY EQPNOME, EQPCOD, LOCCOD, LOCNOME, DPRCOD, DPRREF, DPRHROPER
+#                 UNION
+#                 SELECT 0 TIPO, DPRCOD, DPRREF, EQPCOD, EQPNOME, LOCCOD, LOCNOME, SUM(ADTRPESOTOT) TOTAL, (DPRHRPROD) TOTAL_HORAS FROM ALIMDIARIATRANSP
+#                 JOIN ITEMDIARIATRANSP ON IDTRCOD = ADTRIDTR
+#                 JOIN DIARIATRANSP ON DTRCOD = IDTRDTR
+#                 JOIN LOCAL LD ON LD.LOCCOD = ADTRLOC
+#                 JOIN DIARIAPROD ON DPRCOD = IDTRDPR
+#                 JOIN EQUIPAMENTO ON EQPCOD = DPREQP
+#                 WHERE DTRSIT = 1
+#                 AND CAST(DTRDATA1 as date) BETWEEN '{data_inicio_dt.strftime('%Y-%m-%d')}' AND '{dia_fim.strftime('%Y-%m-%d')}'
+#                 AND IDTRTIPODEST = 1
+#                 AND DTREMP = 1
+#                 AND DTRFIL = 0
+#                 GROUP BY EQPNOME, EQPCOD, LOCCOD, LOCNOME, DPRCOD, DPRREF, DPRHRPROD
+#                 ORDER BY 5,7
+#             """, connections[connection_name])
+
+#             # Adicionando resultados diários
+#             resultados_diarios.append({
+#                 'data': data_inicio_dt.strftime('%Y-%m-%d'),
+#                 'volume_britado': consulta_volume_britado.to_dict(orient='records')
+#             })
+            
+#             # Incrementar para o próximo dia
+#             data_inicio_dt = dia_fim
+
+#         return JsonResponse({'resultados_diarios': resultados_diarios}, status=200)
+
+#     elif tipo_calculo == 'anual':
+#         # Quebrar o anual em mensal
+#         data_inicio = datetime.now().strftime('%Y-01-01 00:00:00')  # Início do ano
+#         data_fim = datetime.now().strftime('%Y-%m-%d 23:59:59')  # Data atual
+        
+#         # Itera sobre os meses do ano
+#         data_inicio_dt = datetime.strptime(data_inicio, '%Y-%m-%d %H:%M:%S')
+#         data_fim_dt = datetime.now()
+#         resultados_mensais = []
+
+#         while data_inicio_dt <= data_fim_dt:
+#             ultimo_dia_mes = (data_inicio_dt.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
+#             data_fim_mes = min(ultimo_dia_mes, data_fim_dt)
+
+#             consulta_volume_britado = pd.read_sql(f"""
+#                 SELECT 0 TIPO, DPRCOD, DPRREF, EQPCOD, EQPNOME, LOCCOD, LOCNOME, SUM(ADPRPESOTOT) TOTAL, (DPRHROPER) TOTAL_HORAS FROM ALIMDIARIAPROD
+#                 JOIN DIARIAPROD ON DPRCOD = ADPRDPR
+#                 JOIN LOCAL LD ON LD.LOCCOD = ADPRLOC
+#                 JOIN EQUIPAMENTO ON EQPCOD = DPREQP
+#                 WHERE DPRSIT = 1
+#                 AND DPREMP = 1
+#                 AND DPRFIL = 0
+#                 AND CAST(DPRDATA1 as date) BETWEEN '{data_inicio_dt.strftime('%Y-%m-%d')}' AND '{data_fim_mes.strftime('%Y-%m-%d')}'
+#                 AND ADPRLOC <> 0
+#                 GROUP BY EQPNOME, EQPCOD, LOCCOD, LOCNOME, DPRCOD, DPRREF, DPRHROPER
+#                 UNION
+#                 SELECT 0 TIPO, DPRCOD, DPRREF, EQPCOD, EQPNOME, LOCCOD, LOCNOME, SUM(ADTRPESOTOT) TOTAL, (DPRHRPROD) TOTAL_HORAS FROM ALIMDIARIATRANSP
+#                 JOIN ITEMDIARIATRANSP ON IDTRCOD = ADTRIDTR
+#                 JOIN DIARIATRANSP ON DTRCOD = IDTRDTR
+#                 JOIN LOCAL LD ON LD.LOCCOD = ADTRLOC
+#                 JOIN DIARIAPROD ON DPRCOD = IDTRDPR
+#                 JOIN EQUIPAMENTO ON EQPCOD = DPREQP
+#                 WHERE DTRSIT = 1
+#                 AND CAST(DTRDATA1 as date) BETWEEN '{data_inicio_dt.strftime('%Y-%m-%d')}' AND '{data_fim_mes.strftime('%Y-%m-%d')}'
+#                 AND IDTRTIPODEST = 1
+#                 AND DTREMP = 1
+#                 AND DTRFIL = 0
+#                 GROUP BY EQPNOME, EQPCOD, LOCCOD, LOCNOME, DPRCOD, DPRREF, DPRHRPROD
+#                 ORDER BY 5,7
+#             """, connections[connection_name])
+
+#             # Adicionando resultados mensais
+#             resultados_mensais.append({
+#                 'mes': data_inicio_dt.strftime('%Y-%m'),
+#                 'volume_britado': consulta_volume_britado.to_dict(orient='records')
+#             })
+
+#             # Incrementar para o próximo mês
+#             data_inicio_dt = (data_inicio_dt.replace(day=28) + timedelta(days=4)).replace(day=1)
+
+#         return JsonResponse({'resultados_mensais': resultados_mensais}, status=200)
+
+#     else:
+#         return JsonResponse({'error': 'Tipo de cálculo inválido'}, status=400)
+
+
+
+
+
