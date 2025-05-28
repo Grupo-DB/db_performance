75ce3b294e67944d84a0c1a2bdd81e9400a61b6a
Grupo-DB <dbrepositorio@outlook.com>
2025-05-28 11:59:42 -0300

    dia28/05

--- controleQualidade/plano/serializers.py
+++ controleQualidade/plano/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
-
+from controleQualidade.ensaio.serializers import EnsaioSerializer
+from controleQualidade.calculosEnsaio.serializers import CalculoEnsaioSerializer
 from controleQualidade.ensaio.models import Ensaio
+from controleQualidade.calculosEnsaio.models import CalculoEnsaio
 from .models import PlanoAnalise
 
+class FlexibleManyRelatedField(serializers.PrimaryKeyRelatedField):
+    def to_internal_value(self, data):
+        if isinstance(data, list):
+            return [super().to_internal_value(item) for item in data]
+        return [super().to_internal_value(data)]
+    
 class PlanoAnaliseSerializer(serializers.ModelSerializer):
-    ensaios = serializers.PrimaryKeyRelatedField(queryset=Ensaio.objects.all(), write_only=True, many=True)
-    calculos_ensaio = serializers.PrimaryKeyRelatedField(queryset=PlanoAnalise.objects.all(), write_only=True, many=True)
-    ensaio_detalhes = serializers.PrimaryKeyRelatedField(source='ensaios', read_only=True)
-    calculo_ensaio_detalhes = serializers.PrimaryKeyRelatedField(source='calculos_ensaio', read_only=True)
+    ensaios = FlexibleManyRelatedField(queryset=Ensaio.objects.all(), write_only=True)
+    calculos_ensaio = FlexibleManyRelatedField(queryset=CalculoEnsaio.objects.all(), write_only=True)
+    ensaio_detalhes = EnsaioSerializer(source='ensaios', read_only=True, many=True)
+    calculo_ensaio_detalhes = CalculoEnsaioSerializer(source='calculos_ensaio', read_only=True, many=True)
     class Meta:
         model = PlanoAnalise
         fields = '__all__'
