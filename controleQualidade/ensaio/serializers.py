65d6b4d2f555deb71c5ec528ce2d5dbf3ae82fae
Grupo-DB <dbrepositorio@outlook.com>
2025-07-18 17:46:39 -0300

    dia 18/07

--- controleQualidade/ensaio/serializers.py
+++ controleQualidade/ensaio/serializers.py
@@ -12,14 +12,15 @@ class VariavelSerializer(serializers.ModelSerializer):
 class EnsaioSerializer(serializers.ModelSerializer):
     tipo_ensaio = serializers.PrimaryKeyRelatedField(queryset=TipoEnsaio.objects.all(), write_only=True)
     tipo_ensaio_detalhes = TipoEnsaioSerializer(source='tipo_ensaio', read_only=True)
-    variavel = serializers.PrimaryKeyRelatedField(queryset=Variavel.objects.all(), write_only=True)
+    variavel = serializers.PrimaryKeyRelatedField(queryset=Variavel.objects.all(), write_only=True, many=True)
     variavel_detalhes = VariavelSerializer(source='variavel', read_only=True, many=True)
     class Meta:
         model = Ensaio
-        #fields = '__all__'
-        fields = ['id', 'descricao', 'responsavel', 'valor', 'tipo_ensaio','tipo_ensaio_detalhes','tempo_previsto','variavel','variavel_detalhes','funcao']
+        fields = '__all__'
+        #fields = ['id', 'descricao', 'responsavel', 'valor', 'tipo_ensaio','tipo_ensaio_detalhes','tempo_previsto','variavel','variavel_detalhes','funcao']
 
     def create(self, validated_data):
-        total = Ensaio.objects.count() + 1
-        validated_data['variavel'] = f'var{total}'
-        return super().create(validated_data)            
+        variaveis = validated_data.pop('variavel', [])
+        ensaio = Ensaio.objects.create(**validated_data)
+        ensaio.variavel.set(variaveis)
+        return ensaio            
--- controleQualidade/ensaio/views.py
+++ controleQualidade/ensaio/views.py
@@ -18,17 +18,6 @@ class TipoEnsaioViewSet(viewsets.ModelViewSet):
         self.perform_update(serializer)
         return Response(serializer.data)
     
-class EnsaioViewSet(viewsets.ModelViewSet):
-    queryset = Ensaio.objects.all()
-    serializer_class = EnsaioSerializer
-    
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
-
 class VariavelViewSet(viewsets.ModelViewSet):
     queryset = Variavel.objects.all()
     serializer_class = VariavelSerializer
@@ -38,4 +27,23 @@ class VariavelViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
+        return Response(serializer.data)    
+    
+class EnsaioViewSet(viewsets.ModelViewSet):
+    queryset = Ensaio.objects.all()
+    serializer_class = EnsaioSerializer
+    
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
         return Response(serializer.data)
+
